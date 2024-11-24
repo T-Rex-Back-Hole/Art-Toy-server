752c0e81f08f1aca93d9ad2a62294ff649b56b66
nuttipol <nuttipol.r@gmail.com>
2024-11-24 17:30:57 +0700

    register and login

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const cors = require ("cors")
+const authRouter= require("./routes/auth/auth-routes")
 
 mongoose
 .connect('mongodb+srv://trexblackhole:trexblackhole1234@cluster0.eiyfe.mongodb.net/')
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-      origin: 'http://localhost:5173/',
+      origin: 'http://localhost:5173',
       methods: ["GET", "POST", "DELETE", "PUT"],
       allowedHeaders: [
         "Content-Type",
@@ -21,6 +22,7 @@ app.use(
         "Cache-Control",
         "Expires",
         "Pragma",
+        
       ],
       credentials: true,
     })
@@ -28,5 +30,6 @@ app.use(
 
   app.use(cookieParser());
 app.use(express.json());
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
