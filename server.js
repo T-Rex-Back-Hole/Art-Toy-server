233b77e162a244cc82ee60ddfac6d62255d5ef53
04_burapa(blue/บลู) <burapa.pratumchai@gmail.com>
2024-12-05 21:30:12 +0700

    connectmongodb

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
 const dotenv = require("dotenv");
 
-// dotenv.config({path: './.env'})
+dotenv.config({path: './.env'})
 
-// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-// mongoose.connect(DB).then(() =>{
-//   console.log('DB Connections Successful!')
-// }).catch((error) => console.log(error));
+/*const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD).replace('<USERNAME>', process.env.DATABASE_USERNAME);
+mongoose.connect(DB).then(() =>{
+console.log('DB Connections Successful!')
+}).catch((error) => console.log(error));*/
 
 mongoose
   .connect(
-    "mongodb+srv://trexblackhole:trexblackhole1234@cluster0.eiyfe.mongodb.net/"
+    process.env.DATABASE
   )
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log("MongoDB connected Successful!"))
   .catch((error) => console.log(error));
 
 const app = express();
